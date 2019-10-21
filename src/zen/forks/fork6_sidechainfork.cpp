526d9aab1dc5b4954b7f3fa7f4f29cf97c6de5c0
Alberto Sala <alberto.sala@nttdata.com>
2019-10-21 16:16:13 +0200

    Added height for sc fork in testnet

--- src/zen/forks/fork6_sidechainfork.cpp
+++ src/zen/forks/fork6_sidechainfork.cpp
@@ -6,7 +6,7 @@ SidechainFork::SidechainFork()
 {
     setHeightMap({{CBaseChainParams::Network::MAIN,655555}, // TODO set it properly
                   {CBaseChainParams::Network::REGTEST,220},
-                  {CBaseChainParams::Network::TESTNET,569900}}); // TODO set it properly
+                  {CBaseChainParams::Network::TESTNET,530250 }});
 
 }
 
