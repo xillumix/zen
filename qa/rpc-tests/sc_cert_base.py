12001906badd31ce217cce192dcb37c1be6cfc26
abi87 <alberto.benegiamo@gmail.com>
2020-02-28 12:04:06 +0100

    Imported python tests. All green except sc_create and sc_cert_invalidate

--- qa/rpc-tests/sc_cert_base.py
+++ qa/rpc-tests/sc_cert_base.py
@@ -65,8 +65,7 @@ class sc_cert_base(BitcoinTestFramework):
         bal_before = self.nodes[1].getbalance("", 0)
         mark_logs("Node1 balance before SC creation: {}".format(bal_before), self.nodes, DEBUG_MODE)
 
-        amounts = [{"address": "dada", "amount": creation_amount}]
-        creating_tx = self.nodes[1].sc_create(scid, EPOCH_LENGTH, amounts)
+        creating_tx = self.nodes[1].sc_create(scid, EPOCH_LENGTH, "dada", creation_amount, "abcdef");
         mark_logs("Node 1 created the SC spending {} coins via tx {}.".format(creation_amount, creating_tx), self.nodes, DEBUG_MODE)
         self.sync_all()
 
